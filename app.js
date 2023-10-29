57619691ee5a448487e185b15d5a7efbba21409a
eleensmathew <eleensmathew@gmail.com>
2023-10-29 14:37:04 +0530

    added update and delete blogs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Blog=require('./models/blogs')
 const app= express();
 
 const dbURI="mongodb+srv://eleena:eleena@blognode.yttzmby.mongodb.net/blog-node?retryWrites=true&w=majority"
-mongoose.connect(dbURI,{useUnifiedTopology: true })
+mongoose.connect(dbURI,{useUnifiedTopology: true })  //help to receive cleaner data otherwise receive it as undefined.
     .then((result) => app.listen(3000))
     .catch((err)=> console.log(err));
 //register view engine
@@ -15,6 +15,7 @@ app.set('view engine','ejs');//automaitically looks in views
 
 
 app.use(express.static('public')); //setting up static files location
+app.use(express.urlencoded({extended:true}));  
 app.use(morgan('dev'));
 
 // app.use((req,res,next)=>{
@@ -63,24 +64,67 @@ app.get('/single-blog',(req,res)=>{
 app.get('/',(req,res)=>{
     //res.send('<p> home page</p>');
     //res.sendFile('./views/index.html',{ root:__dirname });//absolute path
-    const blogs=[
-        {title:'Eleena is amazing', snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
-        {title:'Eleena is not amazing',snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
-    ];
-
-    res.render('index',{title:'Home',blogs});
+    // const blogs=[
+    //     {title:'Eleena is amazing', snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    //     {title:'Eleena is not amazing',snippet:'Lorem ipsum dolor sit amet, consectetur adipiscing elit'},
+    // ];
+    //res.render('index',{title:'Home',blogs});
+    res.redirect('/blogs');
 });
 app.get('/about',(req,res)=>{
     //res.send('<p> about page</p>');
     //res.sendFile('./views/about.html',{ root:__dirname });
     res.render('about',{title:'About'});
 });
+app.get('/blogs',(req,res)=>{
+    Blog.find().sort({createdAt:-1})//descing order 
+        .then((result)=>{
+            res.render('index',{title:'All BLogs',blogs:result })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+app.post('/blogs',(req,res)=>{
+    console.log(req.body);
+    const blog=new Blog(req.body);
+    blog.save()
+        .then((result)=>{
+            res.redirect('/blogs');
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
 app.get('/about-us',(req,res)=>{ //also automatically set the status code
     res.redirect('/about');
 });
+
 app.get('/blogs/create',(req,res)=>{
     res.render('create',{title:'create'});
 });
+app.get('/blogs/:id',(req,res)=>{
+    const id=req.params.id;
+    console.log(id);
+    Blog.findById(id)
+    .then(result=>{
+        res.render('details',{blog:result,title:'Blog details'});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
+app.delete('/blogs/:id',(req,res)=>{
+    const id=req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result=>{
+        res.json({redirect: '/blogs'})
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
 app.use((req,res)=>{ //we put 404 at the end of the routes simillar to default case of switch
     //res.statusCode(404).sendFile('./views/404.html',{ root:__dirname });
     res.status(404).render('404',{title:'404'});
